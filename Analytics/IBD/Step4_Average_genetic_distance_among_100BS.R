7a12d7c5fffa76fe85f2fdb53f04f932075692c9
AFSands <francis.sands@yahoo.com>
2024-05-16 13:07:06 +0800

    Update Step4_Average_genetic_distance_among_100BS.R

--- Analytics/IBD/Step4_Average_genetic_distance_among_100BS.R
+++ Analytics/IBD/Step4_Average_genetic_distance_among_100BS.R
@@ -1,5 +1,6 @@
 ## NOTE1: R script to average 100 bootstrap replicates of genetic distance matrices
 ## NOTE2: Requires the output of Step 3 as an input (see Step4_outI.dist as an example).
+#########################################################################
 
 all.data = c("outI")
 setwd("IBD/")
